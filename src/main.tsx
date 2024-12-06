8301b9023ee5d1bd562aad81fea07bcb158684c4
Chernenko1 <kchek816@gmail.com>
2024-12-06 15:00:15 +0300

    feat: add router

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,21 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import './fonts.css'
 import App from './App.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { HomePage } from '@pages/Home/HomePage.tsx'
+import Shop from '@pages/Shop/Shop.tsx'
+import About from '@pages/About/About.tsx'
+import Contact from '@pages/Contact/Contact.tsx'
+
+const router = createBrowserRouter([
+  { path: '/', element: <HomePage /> },
+  { path: '/shop', element: <Shop /> },
+  { path: '/contact', element: <Contact /> },
+  { path: '/about', element: <About /> },
+])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
