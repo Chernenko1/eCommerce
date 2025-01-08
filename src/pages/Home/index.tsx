8d902eaec5e9466d5c88a4c487f5893165695fa8
Chertenko1 <kchek816@gmail.com>
2025-01-08 10:09:35 +0300

    del: delete old files

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,7 @@
-import { Banner } from '@pages/Home/components/Banner/Banner'
-import HomePageCategories from '@pages/Home/components/HomePageCategories/HomePageCategories'
-import OurProducts from '@pages/Home/components/OurProducts/OurProducts'
-
 export const HomePage = () => {
   return (
     <>
-      <Banner />
-      <HomePageCategories />
-      <OurProducts />
+      <div>1</div>
     </>
   )
 }
