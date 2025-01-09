d90df232a42875252de8887f30361135b6c368d9
Chertenko1 <kchek816@gmail.com>
2025-01-09 11:06:05 +0300

    style: centered contaner

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,11 +2,13 @@ import styles from './header.module.css'
 import shopLogo from '@assets/logo.svg'
 
 import HeaderTop from './components/HeaderTop/HeaderTop'
+import HeaderCenter from './components/HeaderCenter/HeaderCenter'
 
 export const Header = () => {
   return (
-    <header className={styles.mainHeader}>
+    <header className={styles.container}>
       <HeaderTop />
+      <HeaderCenter />
     </header>
   )
 }
