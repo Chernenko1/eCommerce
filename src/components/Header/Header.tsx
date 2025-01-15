1afdd15ee5f5436baad5a131d7a3c49150430120
Chertenko1 <kchek816@gmail.com>
2025-01-15 16:02:02 +0300

    upd: add header for phones

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,12 +3,18 @@ import shopLogo from '@assets/logo.svg'
 
 import HeaderTop from './components/HeaderTop/HeaderTop'
 import HeaderCenter from './components/HeaderCenter/HeaderCenter'
+import PhoneHeader from './components/PhoneHeader/PhoneHeader'
 
 export const Header = () => {
   return (
     <header className={styles.container}>
-      <HeaderTop />
-      <HeaderCenter />
+      <div className={styles.normalScreenHeader}>
+        <HeaderTop />
+        <HeaderCenter />
+      </div>
+      <div className={styles.phoneHeader}>
+        <PhoneHeader />
+      </div>
     </header>
   )
 }
