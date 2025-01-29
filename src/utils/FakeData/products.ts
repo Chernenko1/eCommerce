efb6fff6a1cfba44f7eee9ad03f1c140e3d4d682
Chertenko1 <kchek816@gmail.com>
2025-01-29 15:42:19 +0300

    feat: fake info

---
+++ src/utils/FakeData/products.ts
@@ -0,0 +1,71 @@
+interface IPRODUCTS {
+  img: string | null;
+  title: string;
+  shortDescription?: string;
+  description: string;
+  price: number;
+  saleProce?: number
+}
+
+export const SALEPRODUCTS:IPRODUCTS[] = [
+  {
+  img: '@assets/images/Products/toy.jpg',
+  title: 'SPÄСKUGGARE',
+  shortDescription: 'БАНАН',
+  description: 'Мягкая игрушка банан, желтый/синий, 55 см',
+  price: 19.90,
+  saleProce: 31.00
+},
+{
+  img: null,
+  title: 'VARMDO',
+  shortDescription: 'НАБОР КОРЗИН',
+  description: 'в наборе - 3шт.',
+  price: 62.90,
+  saleProce: 88.00
+},
+{
+  img: '@assets/images/Products/toy.jpg',
+  title: 'SPÄСKUGGARE',
+  shortDescription: 'БАНАН',
+  description: 'Мягкая игрушка банан, желтый/синий, 55 см',
+  price: 19.90,
+  saleProce: 31.00
+},{
+  img: '@assets/images/Products/toy.jpg',
+  title: 'SPÄСKUGGARE',
+  shortDescription: 'БАНАН',
+  description: 'Мягкая игрушка банан, желтый/синий, 55 см',
+  price: 19.90,
+  saleProce: 31.00
+},{
+  img: '@assets/images/Products/toy.jpg',
+  title: 'SPÄСKUGGARE',
+  shortDescription: 'БАНАН',
+  description: 'Мягкая игрушка банан, желтый/синий, 55 см',
+  price: 19.90,
+  saleProce: 31.00
+},{
+  img: '@assets/images/Products/toy.jpg',
+  title: 'SPÄСKUGGARE',
+  shortDescription: 'БАНАН',
+  description: 'Мягкая игрушка банан, желтый/синий, 55 см',
+  price: 19.90,
+  saleProce: 31.00
+},{
+  img: '@assets/images/Products/toy.jpg',
+  title: 'SPÄСKUGGARE',
+  shortDescription: 'БАНАН',
+  description: 'Мягкая игрушка банан, желтый/синий, 55 см',
+  price: 19.90,
+  saleProce: 31.00
+},
+{
+  img: '@assets/images/Products/toy.jpg',
+  title: 'SPÄСKUGGARE',
+  shortDescription: 'БАНАН',
+  description: 'Мягкая игрушка банан, желтый/синий, 55 см',
+  price: 19.90,
+  saleProce: 31.00
+},
+ ]
