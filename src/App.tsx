ac4f63f15d3791490883f2e33c7d2c5367a82dd8
Chertenko1 <kchek816@gmail.com>
2025-02-03 12:00:04 +0300

    add: new pages routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Shop from '@pages/Shop/Shop.tsx'
 import About from '@pages/About/About.tsx'
 import Contact from '@pages/Contact/Contact.tsx'
 import Layout from './Layout/Layout'
 import { HomePage } from '@pages/Home'
+import Delivery from '@pages/Delivery/Delivery'
+import Payment from '@pages/Payment/Payment'
+import WarrantyRefund from '@pages/Warranty and Refund/WarrantyRefund'
+import Stock from '@pages/Stock/Stock'
 
 function App() {
   return (
@@ -11,8 +14,11 @@ function App() {
       <Routes>
         <Route path='/' element={<Layout />}>
           <Route index element={<HomePage />} />
-          <Route path='shop' element={<Shop />} />
+          <Route path='stock' element={<Stock />} />
           <Route path='contact' element={<Contact />} />
+          <Route path='delivery' element={<Delivery />} />
+          <Route path='payment' element={<Payment />} />
+          <Route path='warranty' element={<WarrantyRefund />} />
           <Route path='about' element={<About />} />
         </Route>
       </Routes>
